3a1ebf81d860936d9818979a5b6dd37bb8faea68
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-17 20:10:50 +0530

    update

--- src/component/routing.js
+++ src/component/routing.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouer, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../container/Home';
 import Header from './Header';
 import Footer from './Footer';
 
 const Routing = () => {
     return (
-        <BrowserRouer>
+        <BrowserRouter>
             <Header />
             <Route path="/" component={Home}></Route>
             <Footer />
-        </BrowserRouer>
+        </BrowserRouter>
     )
 }
 
