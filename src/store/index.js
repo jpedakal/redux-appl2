e429ffd948429425cb45ffd72b203f76926cd747
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-17 18:09:30 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,2 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import promiseMiddleware from 'redux-promise';
+
+import reducers from '../reducers';
+
+let store= createStore(reducers, applyMiddleware(promiseMiddleware));
