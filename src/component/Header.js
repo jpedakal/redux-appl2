23032dfb95c1a5f91e04115b183cc3bc5c0a1076
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-17 20:13:21 +0530

    Update Header.js

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <p>&copy; NareshIT  </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
