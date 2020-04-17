3634c909bdeb32da2b6e845bcf7bd8e49f2b8355
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-17 19:29:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './component/routing';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routing />
+  </Provider>
+  , document.getElementById('root'));
