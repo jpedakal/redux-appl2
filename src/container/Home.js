38dacc4092542ace677be408485e0567474c0509
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-18 11:07:37 +0530

    Update Home.js

---
+++ src/container/Home.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+    render() {
+        return (
+            <Fragment>
+                Home
+            </Fragment>
+        )
+    }
+}
+
+export default Home;
