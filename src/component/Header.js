1c5b331ebc4e229948f0e1eea9272810dc61584b
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-18 10:57:09 +0530

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header>
             <div>
-                <p>&copy; NareshIT  </p>
+                <Link to="/">Home</Link>
             </div>
         </header>
     )
